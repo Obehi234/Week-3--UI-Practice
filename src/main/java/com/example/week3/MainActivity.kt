4c497d7f8df2f9591b67cfd5000b6a1ab0bbe895
Obehi234 <48982222+Obehi234@users.noreply.github.com>
2023-02-12 02:00:58 +0100

    Week 3- UI Challenge

---
+++ src/main/java/com/example/week3/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.week3
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
